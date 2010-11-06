59525cb2fc42c105a19150d24eb95e4d55915196
Lindsay Evans <linz@linz.id.au>
2010-11-07 10:47:25 +1100

    Commenting bootstrap load function

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,21 +1,25 @@
 var BWX = {
 	load: function(uris, type, ctx, success){
-		var d = document,
-				type = type || 'text/javascript',
-		    ctx = ctx || d.getElementsByTagName('head')[0],
-				success = success || function(){return arguments;},
+		var d = document, // Cache document
+				type = type || 'text/javascript', // Default type to JS
+		    ctx = ctx || d.getElementsByTagName('head')[0], // Default context to <head>
+				success = success || function(){return arguments;}, // Default success function
 				node, node_type = 'script',
 				i = 0, uris_length = uris.length, uri, loaded = 0
 		;
 
+		// Loop over URIs & append an <object> to context - preloads the resource
 		for(i; i < uris_length; i++){
 			uri = uris[i];
 			node = d.createElement('object');
 			node.data = uri;
 			ctx.appendChild(node);
 
+			// When the resource has loaded, remove the node & increment counter
 			node.onload = function(){
 				ctx.removeChild(this);
+
+				// If all the resources have been loaded, loop over URIs & append the relevant element to context
 				if(++loaded === uris_length){
 					for(i = 0; i < uris_length; i++){
 						uri = uris[i];
@@ -23,6 +27,8 @@ var BWX = {
 						node.src = uri;
 						ctx.appendChild(node);
 					}
+
+					// Call the success function
 					return success.call(uris, type, ctx, success);
 				}
 			}
