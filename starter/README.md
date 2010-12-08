73fbb5af199f51baf8ae727e30e98e6cb149ee09
Lindsay Evans <linz@linz.id.au>
2010-12-09 06:54:42 +1100

    Updating JS libs

--- starter/README.md
+++ starter/README.md
@@ -1,6 +1,7 @@
 #Starter kit containing common files
-+ HTML5 template
-+ jQuery 1.4.2 (minified)
++ HTML5 template (including HTML5 shiv)
++ jQuery 1.4.4 (minified)
++ Underscore 1.1.3 (minified)
 + JS module skeleton
 + screen & print CSS files
 + a nifty empty images directory!
--- starter/js/html5-shiv.js
+++ starter/js/html5-shiv.js
@@ -1,1 +1,1 @@
-/*@cc_on (function(){var e=['abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'],i=21,d=document;while(i--)d.createElement(e[i])})()@*/
+/*@cc_on (function(d,i,e){while(i--)d.createElement(e[i])})(document,21,['abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'])@*/
