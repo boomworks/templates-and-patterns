53a779be4a1996469cab675ebb902ca895862f96
Lindsay Evans <linz@linz.id.au>
2011-05-26 14:36:55 +1000

    Updating application JS to latest bits

--- starter/js/application-name.js
+++ starter/js/application-name.js
@@ -1,33 +1,53 @@
-var APPLICATION_NAME = (function(_this, $){
+/*!
+ * Application_name 0.0.1 - Description
+ *
+ * Copyright (c) 2011 Boomworks <http://boomworks.com.au/>
+ * Licensed under the MIT <http://www.opensource.org/licenses/mit-license.php)> license.
+ */
 
-	// Public properties
+var Application_name = (function(_this, $){
+
+	// Metadata
+	_this.type = 'application';
+	_this.name = 'Application_name';
 	_this.major_version = 0;
 	_this.minor_version = 0;
-	_this.revision = 1;
-	_this.version = [_this.major_version, _this.minor_version, _this.revision].join('.');
+	_this.patch_version = 2;
+	_this.special_version = '';
+	_this.version = '0.0.1';
+	_this.globals = ['Application_name'];
+
+
+	// Public properties
+	_this.bar = 'bar';
+
 
 	// Constructor
 	_this.initialise = function(){
 		$(function(){
-			console.log('APPLICATION_NAME v'+_this.version);
+			console.log('Application_name v'+_this.version);
 		});
 		return _this;
 	};
+
 
 	// Public methods
 	_this.foo = function(){
 		return 'foo';
 	};
 
+
 	// Private properties
 	var _rockin = 'damn straight';
+
 
 	// Private methods
 	var _bar = function(){
 		return 'bar';
 	};
 
+
 	return _this.initialise();
 
-}(APPLICATION_NAME || {}, jQuery));
+}(Application_name || {}, jQuery));
 
